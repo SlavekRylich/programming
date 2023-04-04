e2ccf12af3f7567cf3a881f0a771a35291d6b2d2
Slavek Rylich <xrylic00@vutbr.cz>
2023-04-04 22:51:48 +0200

    start with GUI, navigate to next page dont works

---
+++ C#/game_2048_WFP/game2048/ManuPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace game2048
+{
+    /// <summary>
+    /// Interakční logika pro ManuPage.xaml
+    /// </summary>
+    public partial class ManuPage : Page
+    {
+        public ManuPage()
+        {
+            InitializeComponent();
+        }
+
+        private void New_Game_Button_Click(object sender, RoutedEventArgs e)
+        {
+            Uri uri = new Uri("GamePage.xaml", UriKind.Relative);
+            this.NavigationService.Navigate(uri);
+            //NavigationService nav = NavigationService.GetNavigationService(this);
+            //nav.Navigate(new Uri("GamePage.xaml", UriKind.RelativeOrAbsolute));
+        }
+    }
+}
